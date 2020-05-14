6f35ce3f867a09d4c844ec4324d2e69c2fd4960e
Tobias Klein <contact@tklein.info>
2020-05-14 20:54:32 +0200

    Use deb_2004 for building Ubuntu 20.04 package

--- docker/build_ubuntu_2004.sh
+++ docker/build_ubuntu_2004.sh
@@ -2,5 +2,5 @@
 # This script triggers all the steps necessary for building and packaging Ezra Project on Linux.
 
 npm run build-linux
-npm run deb_1910
+npm run deb_2004
 cp release/packages/*.deb $GITHUB_WORKSPACE/ezra-project_latest.deb
