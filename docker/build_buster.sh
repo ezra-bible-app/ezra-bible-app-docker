a93a42ff096ff4ed7a7d9ec8638cca1ede020a22
Tobias Klein <contact@tklein.info>
2020-06-07 08:44:18 +0200

    Add experimental console output to build_buster.sh

--- docker/build_buster.sh
+++ docker/build_buster.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 # This script triggers all the steps necessary for building and packaging Ezra Project on Linux.
 
+echo "GITHUB_EVENT_NAME"
+echo $GITHUB_EVENT_NAME
+echo "GITHUB_REF"
+echo $GITHUB_REF
+
 npm run build-linux
 npm run deb_buster
 cp release/packages/*.deb $GITHUB_WORKSPACE/ezra-project_latest.deb
