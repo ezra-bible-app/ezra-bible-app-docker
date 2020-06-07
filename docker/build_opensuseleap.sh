5fb245b48701dc78623a29917be0356a38284cc8
Tobias Klein <contact@tklein.info>
2020-06-07 09:34:05 +0200

    Add sentry upload to all build scripts

--- docker/build_opensuseleap.sh
+++ docker/build_opensuseleap.sh
@@ -4,3 +4,9 @@
 npm run build-linux
 npm run rpm_opensuse_leap
 cp release/packages/*.rpm $GITHUB_WORKSPACE/ezra-project_latest.rpm
+
+if [ "$GITHUB_EVENT_NAME" = "release" ]; then
+  node_modules/.bin/sentry-cli --auth-token $SENTRY_TOKEN \
+    upload-dif -o tobias-klein -p ezra-project \
+    node_modules/node-sword-interface/build/Release/node_sword_interface.node
+fi
--- docker/build_ubuntu_1804.sh
+++ docker/build_ubuntu_1804.sh
@@ -4,3 +4,9 @@
 npm run build-linux
 npm run deb_1804
 cp release/packages/*.deb $GITHUB_WORKSPACE/ezra-project_latest.deb
+
+if [ "$GITHUB_EVENT_NAME" = "release" ]; then
+  node_modules/.bin/sentry-cli --auth-token $SENTRY_TOKEN \
+    upload-dif -o tobias-klein -p ezra-project \
+    node_modules/node-sword-interface/build/Release/node_sword_interface.node
+fi
