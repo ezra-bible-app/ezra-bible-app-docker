321b0e6bd9d072cfc42458e53e3bf66bfdee0b08
Tobias Klein <contact@tklein.info>
2021-08-21 10:57:58 +0200

    Adjust name for fedora packacking command

--- docker/build_fedora31.sh
+++ docker/build_fedora31.sh
@@ -2,7 +2,7 @@
 # This script triggers all the steps necessary for building and packaging Ezra Bible App on Linux.
 
 npm run build-linux
-npm run rpm_fedora29
+npm run rpm_fedora
 cp release/packages/*.rpm $GITHUB_WORKSPACE/ezra-bible-app_latest.rpm
 
 if [ "$GITHUB_EVENT_NAME" = "release" ]; then
