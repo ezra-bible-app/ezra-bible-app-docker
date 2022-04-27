c66929759c7742b213503bcf6c0111307dfb124f
Tobias Klein <contact@tklein.info>
2022-04-27 21:20:36 +0200

    Add Ubuntu 22.04 Dockerfile and build script

---
+++ docker/build_ubuntu_2204.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This script triggers all the steps necessary for building and packaging Ezra Bible App on Linux.
+
+npm run build-linux
+npm run deb_2204
+cp release/packages/*.deb $GITHUB_WORKSPACE/ezra-bible-app_latest.deb
+
+if [ "$GITHUB_EVENT_NAME" = "release" ]; then
+  node_modules/.bin/sentry-cli --auth-token $SENTRY_TOKEN \
+    upload-dif -o tobias-klein -p ezra-bible-app \
+    node_modules/node-sword-interface/build/Release/node_sword_interface.node
+fi
