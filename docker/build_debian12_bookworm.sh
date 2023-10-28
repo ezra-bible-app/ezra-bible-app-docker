412cfdaba8a5a71d663b52e78931871531296658
Tobias Klein <contact@tklein.info>
2023-10-28 12:50:54 +0200

    Update build_debian12_bookworm.sh

--- docker/build_debian12_bookworm.sh
+++ docker/build_debian12_bookworm.sh
@@ -7,6 +7,8 @@ curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv
 export PATH=$PATH:$HOME/.pyenv/bin
 pyenv install 3.10.0
 pyenv global 3.10.0
+pyenv init -
+pyenv virtualenv-init -
 pyenv exec python -m venv .venv
 pyenv activate venv
 
