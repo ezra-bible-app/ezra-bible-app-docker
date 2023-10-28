86e6f2fd3cfd50da76d262dcee93e715ca79dfe3
Tobias Klein <contact@tklein.info>
2023-10-28 10:15:22 +0200

    Use Python 3.10 on Debian 12

--- docker/build_debian12_bookworm.sh
+++ docker/build_debian12_bookworm.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # This script triggers all the steps necessary for building and packaging Ezra Bible App on Linux.
 
+# Use Python 3.10.0, because with 3.11 there are issues with node-gyp
+# See https://stackoverflow.com/questions/74715990/node-gyp-err-invalid-mode-ru-while-trying-to-load-binding-gyp
+pyenv local 3.10.0
+
 npm run build-linux
 npm run deb_bookworm
 cp release/packages/*.deb $GITHUB_WORKSPACE/ezra-bible-app_latest.deb
