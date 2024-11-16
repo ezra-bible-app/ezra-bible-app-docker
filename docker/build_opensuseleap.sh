647e8e3cf00c9a54658c013cceccda3ac80f787e
Tobias Klein <contact@tklein.info>
2024-11-16 10:34:09 +0100

    Remove dependency to atspi in case of OpenSuse 15.6

--- docker/build_opensuseleap.sh
+++ docker/build_opensuseleap.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # This script triggers all the steps necessary for building and packaging Ezra Bible App on Linux.
 
 mkdir -p release/electron-installer-redhat
@@ -7,6 +7,11 @@ git clone https://github.com/electron-userland/electron-installer-redhat release
 # Use specific version of electron-installer-redhat that adds support for OpenSuse 15.6 in terms of dependency setup
 # See https://github.com/electron-userland/electron-installer-redhat/commit/0ca95554b2bdacd24da7942f459709df41336972
 git -C release/electron-installer-redhat checkout 0ca955
+
+# Remove dependency to atspi in case of OpenSuse 15.6
+if grep -q 15.6 "/etc/os-release"; then
+  sed -i '/atspi/d' release/electron-installer-redhat/src/dependencies.js
+fi
 
 npm install --prefix release/electron-installer-redhat
 
