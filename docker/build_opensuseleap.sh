29dd4d4cba39abee6faf1091bf7fcac2c2e364e3
Tobias Klein <contact@tklein.info>
2024-11-16 10:09:04 +0100

    Update OpenSuseLeap containers

--- docker/build_opensuseleap.sh
+++ docker/build_opensuseleap.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # This script triggers all the steps necessary for building and packaging Ezra Bible App on Linux.
 
+mkdir release/electron-installer-redhat
+git clone https://github.com/electron-userland/electron-installer-redhat release/electron-installer-redhat
+npm install --prefix release/electron-installer-redhat
+
 npm run build-linux
 npm run rpm_opensuse_leap
 cp release/packages/*.rpm $GITHUB_WORKSPACE/ezra-bible-app_latest.rpm
